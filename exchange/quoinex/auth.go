a1460fdda9626a18d78494348b3773e01efcc2cb
Lucas Sartore <lucas.sartore@gmail.com>
2018-01-06 12:21:58 +0100

    Prepare Quoinex balance and authentication

--- exchange/quoinex/auth.go
+++ exchange/quoinex/auth.go
@@ -1,1 +1,38 @@
 package quoinex
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func (q *Quoinex) getSignature() string {
+	return ""
+}
+
+func (e *Quoinex) getAuthValues() url.Values {
+	nonce := strconv.FormatInt(time.Now().Unix(), 10)
+
+	message := nonce + e.CustomerID + e.ApiKey
+
+	mac := hmac.New(sha256.New, []byte(e.Secret))
+	mac.Write([]byte(message))
+	macSum := mac.Sum(nil)
+	sig := strings.ToUpper(hex.EncodeToString(macSum))
+
+	form := url.Values{
+		"key":       {e.ApiKey},
+		"signature": {sig},
+		"nonce":     {nonce},
+	}
+	return form
+}
+
+func (e *Quoinex) getSignedRequest(values url.Values) *http.Request {
+	panic("Unimplemented")
+}
--- exchange/quoinex/balance.go
+++ exchange/quoinex/balance.go
@@ -1,5 +1,36 @@
 package quoinex
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 func (e *Quoinex) GetBalance(currency string) float64 {
+	url := "https://api.quoine.com"
+	path := "/accounts/balance"
+	url += path
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		panic(err)
+	}
+	signature := e.getSignature()
+	req.Header.Add("X-Quoine-Auth", signature)
+	req.Header.Add("Content-Type", "application/json")
+	// req.Header.Add("X-Quoine-API-Version", "2")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 	panic("Unimplemented")
 }
