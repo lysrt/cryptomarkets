325cc81b59fabff4683965ae1ab640afa9798b06
Lucas Sartore <lucas.sartore@gmail.com>
2018-01-07 16:08:06 +0100

    Complete Quoinex authentication

--- exchange/quoinex/auth.go
+++ exchange/quoinex/auth.go
@@ -3,7 +3,9 @@ package quoinex
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -11,10 +13,60 @@ import (
 	"time"
 )
 
-func (q *Quoinex) getSignature() string {
-	return ""
+type header struct {
+	Type string `json:"typ"`
+	Algo string `json:"alg"`
 }
 
+type payload struct {
+	Path    string `json:"path"`
+	Nonce   int64  `json:"nonce"`
+	TokenID string `json:"token_id"`
+}
+
+func (q *Quoinex) getSignature(path string) string {
+	tokenId := q.ApiKey
+	secret := q.Secret
+
+	header := header{
+		Algo: "HS256",
+		Type: "JWT",
+	}
+
+	authPayload := payload{
+		Path:    path,
+		Nonce:   time.Now().Unix(),
+		TokenID: tokenId,
+	}
+
+	jsonHeader, err := json.Marshal(header)
+	if err != nil {
+		panic(err)
+	}
+	jsonPayload, err := json.Marshal(authPayload)
+	if err != nil {
+		panic(err)
+	}
+
+	// signature := JWT.encode(authPayload, userSecret, "HS256")
+
+	encodedHeader := base64.StdEncoding.EncodeToString(jsonHeader)
+	encodedPayload := base64.StdEncoding.EncodeToString(jsonPayload)
+
+	// ----- HMAC256
+	message := encodedHeader + "." + encodedPayload
+	// userSecret
+
+	mac := hmac.New(sha256.New, []byte(secret))
+	mac.Write([]byte(message))
+	macSum := mac.Sum(nil)
+
+	signature := base64.StdEncoding.EncodeToString(macSum)
+
+	return encodedHeader + "." + encodedPayload + "." + signature
+}
+
+////////////////////////////////////////////
 func (e *Quoinex) getAuthValues() url.Values {
 	nonce := strconv.FormatInt(time.Now().Unix(), 10)
 
--- exchange/quoinex/balance.go
+++ exchange/quoinex/balance.go
@@ -1,24 +1,32 @@
 package quoinex
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type balance struct {
+	Currency string      `json:"currency"`
+	Balance  json.Number `json:"balance"`
+}
+
 func (e *Quoinex) GetBalance(currency string) float64 {
 	url := "https://api.quoine.com"
 	path := "/accounts/balance"
+	// path := "/crypto_accounts" // More details
 	url += path
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		panic(err)
 	}
-	signature := e.getSignature()
+
+	signature := e.getSignature(path)
 	req.Header.Add("X-Quoine-Auth", signature)
 	req.Header.Add("Content-Type", "application/json")
-	// req.Header.Add("X-Quoine-API-Version", "2")
+	req.Header.Add("X-Quoine-API-Version", "2")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -26,11 +34,24 @@ func (e *Quoinex) GetBalance(currency string) float64 {
 	}
 	defer resp.Body.Close()
 
+	// fmt.Println(resp)
+	// for k, v := range resp.Header {
+	// 	fmt.Println(k, v)
+	// }
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(body))
-	panic("Unimplemented")
+	// fmt.Println("Body:", string(body))
+
+	var balances []balance
+
+	json.Unmarshal(body, &balances)
+	for _, b := range balances {
+		fmt.Println(b.Currency, b.Balance)
+	}
+
+	return 0.0
 }
