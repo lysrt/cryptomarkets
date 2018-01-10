064d8c37cfbbcad6f66e7335831c647690b641e6
Lucas Sartore <lucas.sartore@gmail.com>
2018-01-11 00:05:43 +0100

    Add GDAX ticker

--- example/main.go
+++ example/main.go
@@ -5,8 +5,10 @@ import (
 	"log"
 
 	"github.com/lysrt/cryptomarkets/entity"
-
+	"github.com/lysrt/cryptomarkets/exchange/binance"
 	"github.com/lysrt/cryptomarkets/exchange/bitstamp"
+	"github.com/lysrt/cryptomarkets/exchange/bittrex"
+	"github.com/lysrt/cryptomarkets/exchange/gdax"
 	"github.com/lysrt/cryptomarkets/exchange/quoinex"
 )
 
@@ -18,6 +20,9 @@ func main() {
 	providers := map[string]pricer{
 		"bitstamp": &bitstamp.Bitstamp{},
 		"quoinex":  &quoinex.Quoinex{},
+		"bittrex":  &bittrex.Bittrex{},
+		"binance":  &binance.Binance{},
+		"gdax":     &gdax.Gdax{},
 	}
 
 	bp, err := providers["bitstamp"].GetTicker("BTC", "USD")
@@ -30,6 +35,24 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Bitstamp: ", bp.LastPrice)
+	bip, err := providers["binance"].GetTicker("BTC", "USDT")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	btp, err := providers["bittrex"].GetTicker("USDT", "BTC")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gp, err := providers["gdax"].GetTicker("BTC", "USD")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Bitstamp:", bp.LastPrice)
 	fmt.Println("Quoinex: ", qp.LastPrice)
+	fmt.Println("Binance: ", bip.LastPrice)
+	fmt.Println("Bittrex: ", btp.LastPrice)
+	fmt.Println("GDAX:    ", gp.LastPrice)
 }
--- exchange/gdax/ticker.go
+++ exchange/gdax/ticker.go
@@ -1,11 +1,66 @@
 package gdax
 
 import (
-	"errors"
+	"encoding/json"
+	"fmt"
+	"time"
 
+	"github.com/lysrt/cryptomarkets/common"
 	"github.com/lysrt/cryptomarkets/entity"
 )
 
-func (e *Gdax) GetTicker(from, to string) (*entity.Ticker, error) {
-	return nil, errors.New("unimplemented")
+type gdaxTicker struct {
+	TradeId int64     `json:"trade_id"`
+	Price   float64   `json:"price,string"`
+	Size    float64   `json:"size,string"`
+	Bid     float64   `json:"bid,string"`
+	Ask     float64   `json:"ask,string"`
+	Volume  float64   `json:"volume,string"`
+	Time    time.Time `json:"time"`
 }
+
+func (e *Gdax) GetTicker(from, to string) (*entity.Ticker, error) {
+	currencyPair := entity.Pair{
+		First:  entity.NewCurrency(from),
+		Second: entity.NewCurrency(to),
+	}
+
+	url := fmt.Sprintf("https://api.gdax.com/products/%s/ticker", currencyPair.Upper("-"))
+
+	// TODO Run application/json request
+	body, err := common.RunRequest(url)
+	if err != nil {
+		return nil, fmt.Errorf("%q: %s", err, string(body))
+	}
+
+	var t gdaxTicker
+	err = json.Unmarshal(body, &t)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO can get more data using stats: /products/<product-id>/stats
+	// s.Low, s.High, s.Open, s.Volume (s.Last, s.Volume30Day)
+
+	return &entity.Ticker{
+		Timestamp:     t.Time.Unix(),
+		LastPrice:     t.Price,
+		LastQuantity:  t.Size,
+		High:          0,
+		Low:           0,
+		Open:          0,
+		Close:         0,
+		Ask:           t.Ask,
+		AskQuantity:   0,
+		Bid:           t.Bid,
+		BidQuantity:   0,
+		VWAP:          0,
+		Volume:        t.Volume,
+		QuoteVolume:   0,
+		PriceChange:   0,
+		PercentChange: 0,
+		Pair:          currencyPair,
+	}, nil
+}
+
+// TODO Could use /products to list all available currency pairs
