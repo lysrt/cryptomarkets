17f6a600184c79736fa86ee7343c2cc9dbfdcc61
Lucas Sartore <lucas.sartore@gmail.com>
2018-01-11 00:18:36 +0100

    Satisfy Exchange interface for newly added exchanges

---
+++ exchange/bittrex/balance.go
@@ -0,0 +1,11 @@
+package bittrex
+
+import (
+	"errors"
+
+	"github.com/lysrt/cryptomarkets/entity"
+)
+
+func (e *Bittrex) GetBalance() (*entity.Balance, error) {
+	return nil, errors.New("unimplemented")
+}
